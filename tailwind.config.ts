6aec4f11b53916184a7ff1841ef006746c567b6b
plla2 <jhbljs92@naver.com>
2025-02-03 17:38:51 +0900

    Feat: 폰트 추가

---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import { colors } from './src/styles/global';
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors,
+      textColor: {},
+      translate: {
+        '-1/2': '-50%',
+      },
+      boxShadow: {
+        dropShadow: '0px 8px 16px rgba(0, 0, 0, 0.12)',
+      },
+      transitionProperty: {
+        colors: 'color, background-color, border-color, text-decoration-color',
+      },
+      fontFamily: {
+        GmarketBold: ['GmarketBold'],
+        GmarketMedium: ['GmarketMedium'],
+        GmarketLight: ['GmarketLight'],
+        SometimesBold: ['SometimesBold'],
+        SometimesMedium: ['SometimesMedium'],
+        SometimesLight: ['SometimesLight'],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
