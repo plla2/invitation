b22e5a481b365f92ab03f8f392c151e515b74169
plla2 <jhbljs92@naver.com>
2025-02-04 14:26:06 +0900

    Feat: 첫화면 렌더링 전 overlay, typed텍스트 애니메이션 추가

--- src/components/features/Landing/index.tsx
+++ src/components/features/Landing/index.tsx
@@ -1,11 +1,22 @@
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import { AnimatePresence, motion } from 'motion/react';
+import { useEffect, useState } from 'react';
+import { ReactTyped } from 'react-typed';
 
 const Landing = () => {
   useEffect(() => {
     Aos.init();
   }, []);
+
+  const [showOverlay, setShowOverlay] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowOverlay(false);
+    }, 4000);
+    return () => clearTimeout(timer);
+  });
 
   return (
     <div className="min-h-[650px] relative text-white-100">
@@ -16,18 +27,67 @@ const Landing = () => {
         decoding="async"
         fetchPriority="high"
       />
-      <div data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000">
-        <h1 className="text-4xl w-full break-all absolute z-20 top-16 text-center font-SometimesLight font-light">
-          GILDONG & YURI
-        </h1>
+      <div data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1000">
+        <span className="text-3xl w-full break-all absolute z-20 top-[70px] text-center font-SometimesLight font-light">
+          GUNWOO & HYUNHEE
+        </span>
+        <div className="flex gap-[120px] justify-center text-sm w-full break-all absolute z-20 top-[110px] text-center font-Pretendard font-normal">
+          <div className="flex gap-1">
+            <span>신랑</span>
+            <span>정건우</span>
+          </div>
+          <div className="flex gap-1">
+            <span>신부</span>
+            <span>박현희</span>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-col gap-2 text-lg font-light absolute w-full break-all z-20 text-center items-center tracking-wide font-Pretendard bottom-12">
+      <div
+        data-aos="fade-down"
+        data-aos-easing="linear"
+        data-aos-duration="1000"
+        className="flex flex-col gap-2 text-lg font-light absolute w-full break-all z-20 text-center items-center tracking-wide font-Pretendard bottom-12"
+      >
         <div className="flex gap-2 items-center">
           <span>2025. 02. 04. Tue</span>
           <span>13:00</span>
         </div>
         <span>가나다라마바사아자차카타파하</span>
       </div>
+      <AnimatePresence>
+        {showOverlay && (
+          <motion.div
+            className="absolute inset-0 min-h-dvh flex items-center justify-center bg-black-100 bg-opacity-50 z-50"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <motion.h1
+              className="text-white text-4xl font-bold"
+              initial={{ y: 50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -50, opacity: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <ReactTyped
+                strings={['하고싶은말 정해지면 들어갈 곳']}
+                typeSpeed={150}
+                loop={false}
+                style={{
+                  color: '#FEFEFE',
+                  fontWeight: 'normal',
+                  fontSize: '18px',
+                  placeItems: 'center',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  fontFamily: 'Pretendard',
+                }}
+              />
+            </motion.h1>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
