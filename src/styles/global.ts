95433618d4b4a44d997d566f381d908ada6e3741
plla2 <jhbljs92@naver.com>
2025-02-04 14:25:26 +0900

    Feat: blue colors 추가

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,6 +7,12 @@ export const colors = {
   black: {
     100: '#191919',
   },
+  blue: {
+    100: '#C2D4E5',
+    200: '#D4E1ED',
+    300: '#E6EDF4',
+    400: '#F8FAFC',
+  },
 
   // BLACK: '#191919',
   // WHITE: '#FEFEFE',
@@ -14,10 +20,24 @@ export const colors = {
   // OUTSIDE: '#efefef',
 };
 
+export const weights = {
+  thin: 100,
+  extralight: 200,
+  light: 300,
+  regular: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+  extrabold: 800,
+  black: 900,
+};
+
 export const theme = {
   colors,
+  weights,
 };
 
 export type Colors = keyof typeof colors;
+export type Weights = keyof typeof weights;
 
 export const colorNames = Object.keys(colors) as Colors[];
