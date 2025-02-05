9172fbadfafee84038d67f817557900b076c95a7
plla2 <jhbljs92@naver.com>
2025-02-05 15:43:33 +0900

    Feat: 여러 컴포넌트에서 쓰일 flower 비디오 컴포넌트 작성

---
+++ src/components/common/VideoSection/VideoSection.tsx
@@ -0,0 +1,23 @@
+import { useRef } from 'react';
+import { useInView } from 'framer-motion';
+
+const VideoSection = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const isInView = useInView(videoRef, { once: false, margin: '-10% 0px' });
+
+  if (videoRef.current) {
+    if (isInView) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  }
+
+  return (
+    <div className="flex w-20 h-20 justify-center items-center">
+      <video ref={videoRef} src="/images/flower.mp4" muted playsInline />
+    </div>
+  );
+};
+
+export default VideoSection;
