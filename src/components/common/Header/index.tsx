3b76610e51bd25970b290cb8d9ffa5cd69d58119
plla2 <jhbljs92@naver.com>
2025-02-05 12:43:27 +0900

    Fix: 배경음악 재생, 일시정지 클릭안되는 버그 수정

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -18,13 +18,13 @@ const Header = () => {
   }, [isPlaying, audioElement]);
 
   return (
-    <div className="absolute right-3 top-3 z-30">
+    <div onClick={() => setIsPlaying(!isPlaying)} className="absolute right-3 top-3 z-50 cursor-pointer">
       <audio loop ref={audioElement}>
         <source src="/musics/back-music.mp3" type="audio/mp3" />
       </audio>
-      <button
-        className="w-7 h-7 flex justify-center items-center bg-white-300 rounded-3xl overflow-hidden relative"
-        onClick={() => setIsPlaying(!isPlaying)}
+      <div
+        className="w-7 h-7 flex justify-center items-center bg-white-300 rounded-3xl overflow-hidden"
+        // onClick={() => setIsPlaying(!isPlaying)}
       >
         <AnimatePresence mode="wait">
           {!isPlaying ? (
@@ -51,7 +51,7 @@ const Header = () => {
             </motion.div>
           )}
         </AnimatePresence>
-      </button>
+      </div>
     </div>
   );
 };
