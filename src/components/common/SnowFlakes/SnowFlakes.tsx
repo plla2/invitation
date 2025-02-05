89eb4db067386a3b92b2d6906bad1dc1a47ad87a
plla2 <jhbljs92@naver.com>
2025-02-05 09:45:11 +0900

    Feat: SnowFlakes를 통한 눈송이 애니메이션 스타일 수정

--- src/components/common/SnowFlakes/SnowFlakes.tsx
+++ src/components/common/SnowFlakes/SnowFlakes.tsx
@@ -24,7 +24,8 @@ export default function Snowflakes({ count }: SnowflakesProps) {
       const shakeDelay = Math.min(
         generateRandomNumber(0, 10, { fixed: 1 }),
         Number.parseFloat((fallDelay - 0.07).toFixed(1)),
-      ); // fallDelay보다 무조건 길어야 한다. 그렇지 않으면 일부 구간 눈송이가 일자로 내리게 된다.
+      );
+
       return {
         left: generateRandomNumber(0, 100),
         fallDelay,
@@ -38,17 +39,18 @@ export default function Snowflakes({ count }: SnowflakesProps) {
   }, []);
 
   return (
-    <div className="absolute z-50 left-0 top-0" aria-hidden="true">
+    <div className="absolute z-50 left-0 top-0 w-full h-[650px] overflow-hidden" aria-hidden="true">
       {snowflake.map((flake, index) => (
         <div
           key={`flake-${index}`}
-          className="pointer-events-none text-blue-100 shadow-sm fixed -top-[10%] z-50 select-none cursor-default animate-snow"
+          className="pointer-events-none text-blue-100 shadow-sm absolute select-none cursor-default animate-snow"
           style={{
             left: `${flake.left}%`,
             filter: `blur(${flake.blur}px)`,
             opacity: `${flake.opacity}`,
             animationDelay: `${flake.fallDelay}s, ${flake.shakeDelay}s`,
             WebkitAnimationDelay: `${flake.fallDelay}s, ${flake.shakeDelay}s`,
+            top: '-5%', // 눈이 살짝 위에서 내려오게 설정
           }}
         >
           <Snow size={flake.size} />
