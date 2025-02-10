e155bb306bf7191e515cdac450971169415b4e51
plla2 <jhbljs92@naver.com>
2025-02-10 15:00:48 +0900

    Feat: ToggleListData 상수화

---
+++ src/constants/ToggleListData.ts
@@ -0,0 +1,41 @@
+export const toggleListHusData = [
+  {
+    grade: '신랑',
+    name: '정건우',
+    bank: '국민은행',
+    account: '123-123-1230000',
+  },
+  {
+    grade: '신랑 아버지',
+    name: '아버지',
+    bank: '국민은행',
+    account: '123-123-1230000',
+  },
+  {
+    grade: '신랑 어머니',
+    name: '어머니',
+    bank: '국민은행',
+    account: '123-123-1230000',
+  },
+];
+
+export const toggleListWifeData = [
+  {
+    grade: '신부',
+    name: '박현희',
+    bank: '국민은행',
+    account: '123-123-1230000',
+  },
+  {
+    grade: '신부 아버지',
+    name: '아버지',
+    bank: '국민은행',
+    account: '123-123-1230000',
+  },
+  {
+    grade: '신부 어머니',
+    name: '어머니',
+    bank: '국민은행',
+    account: '123-123-1230000',
+  },
+];
